0b41e621d9d5705e3f30db607d3d97acfb102a20
Maxwel <maxwelsant96@icloud.com>
2021-11-08 16:23:29 -0300

    Setting Picture URL in DynamoDB

--- src/handlers/uploadAuctionPicture.js
+++ src/handlers/uploadAuctionPicture.js
@@ -1,18 +1,29 @@
 import { getAuctionById } from './getAuction';
 import { uploadPictureToS3 } from '../lib/uploadPIctureToS3';
+import middy from '@middy/core';
+import httpErrorHandler from '@middy/http-error-handler';
+import createHttpError from 'http-errors';
+import { setAuctionPictureUrl } from './setAuctionPictureUrl';
 
 export async function uploadAuctionPicture(event) {
   const { id } = event.pathParameters;
   const auction = await getAuctionById(id);
   const base64 = event.body.replace(/^data:image\/\w+;base64,/, '');
   const buffer = Buffer.from(base64, 'base64');
+  let updatedAuction;
 
-  const uploadToS3Result = await uploadPictureToS3(auction.id + '.jpg', buffer);
-  console.log(uploadToS3Result);
+  try {
+    const pictureUrl = await uploadPictureToS3(auction.id + '.jpg', buffer);
+    updatedAuction = await setAuctionPictureUrl(id, pictureUrl);
+  } catch (error) {
+    console.error(error);
+    throw new createHttpError.InternalServerError(error);
+  }
+
   return {
     statusCode: 200,
-    body: JSON.stringify({}),
+    body: JSON.stringify(updatedAuction),
   };
 }
 
-export const handler = uploadAuctionPicture;
+export const handler = middy(uploadAuctionPicture).use(httpErrorHandler());
